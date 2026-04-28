c07ed4d55fa0e1b232d1171f8c115ff5c79d1503
Artem Zatsarnyi <artem.az2005@gmail.com>
2026-04-28 14:28:51 +0200

    t7.1

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,65 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('TodoMVC', () => {
+  beforeEach(() => {
+    // Этот код сработает перед каждым it()
+    cy.visit('https://todomvc-app-for-testing.surge.sh/')
   })
+  
+  it('Crear tarea', () => {
+    cy.get('input.new-todo').type(`Hola{enter}`);
+    cy.contains('label', "Hola").should('be.visible')
+  })
+
+  it('Marcar tarea como completada', () => {
+    cy.get('input.new-todo').type(`Hola{enter}`);
+    cy.get(".todo-list").contains("Hola").parents("li").find('input[type="checkbox"]').check({ force: true }); 
+    cy.get(".todo-list").contains("Hola").parents("li").should("have.class", "completed");
+  })
+
+  it('Desmarcar tarea completada', () => {
+    cy.get('input.new-todo').type(`Hola{enter}`);
+    cy.get(".todo-list").contains("Hola").parents("li").find('input[type="checkbox"]').check({ force: true }); 
+    cy.get(".todo-list").contains("Hola").parents("li").find('input[type="checkbox"]').uncheck({ force: true });
+    cy.get(".todo-list").contains("Hola").parents("li").should("not.have.class", "completed");
+  })
+
+  it('editar tarea', () => {
+    cy.get('input.new-todo').type(`Hola{enter}`);
+    cy.get(".todo-list").contains("Hola").dblclick();
+    cy.get(".edit").clear().type(`Hola hola{enter}`);
+    cy.contains('label', "Hola hola").should('be.visible')
+    
+  })
+
+  it('Eliminar tarea', () => {
+    cy.get('input.new-todo').type(`Hola{enter}`);
+    cy.get(".todo-list").contains("Hola").parents("li").find('.destroy').dblclick({ force: true });
+    cy.get(".todo-list").should('not.be.visible')
+  })
+
+  it('Filtrar tarea', () => {
+    cy.get('input.new-todo').type(`Hola1{enter}`);
+    cy.get(".todo-list").contains("Hola1").parents("li").find('input[type="checkbox"]').check({ force: true }); 
+    cy.get('input.new-todo').type(`Hola2{enter}`);
+    cy.get('input.new-todo').type(`Hola3{enter}`);
+    cy.get('input.new-todo').type(`Hola4{enter}`);
+    cy.get(".todo-list").contains("Hola4").parents("li").find('input[type="checkbox"]').check({ force: true }); 
+
+    cy.get(".footer").contains("Completed").parents("li").click();
+
+    cy.get(".todo-list").contains("Hola1").should("be.visible");
+    cy.get(".todo-list").contains("Hola4").should("be.visible");
+    cy.contains('.todo-list li', 'Hola2').should('not.exist');
+    cy.contains('.todo-list li', 'Hola3').should('not.exist');
+
+    cy.get(".footer").contains("Active").parents("li").click();
+
+    cy.get(".todo-list").contains("Hola2").should("be.visible");
+    cy.get(".todo-list").contains("Hola3").should("be.visible");
+    cy.contains(".todo-list", "Hola1").should("not.exist");
+    cy.contains(".todo-list", "Hola4").should("not.exist");
+    
+    cy.get(".footer").contains("All").click();
+
+  })
+
 })
---
+++ tmp/TodoPage.js
@@ -0,0 +1,25 @@
+class TodoPage {
+  // Selectores
+  elements = {
+    inputNewTodo: () => cy.get('input.new-todo'),
+    todoItemLabel: (itemText) => cy.contains('label', itemText),
+    todoItems: () => cy.get('.todo-list li'),
+    deleteButton: (itemText) =>
+      this.elements.todoItemLabel(itemText).parent().find('button.destroy'),
+  };
+
+  // Métodos
+  addTodo(taskName) {
+    this.elements.inputNewTodo().type(`${taskName}{enter}`);
+  }
+
+  deleteTodo(taskName) {
+    this.elements.deleteButton(taskName).click({ force: true });
+  }
+
+  validateTodoExists(taskName) {
+    this.elements.todoItemLabel(taskName).should('be.visible');
+  }
+}
+
+export default TodoPage;
